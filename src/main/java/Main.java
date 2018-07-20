7325da192e3a571cc57f97a14abaf9e72f533456
Vlad <vladkozachek90@gmail.com>
2018-07-20 12:11:42 +0300

    Создание класса ConnectJDBC для подключения к базе данных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        ConnectJDBC connectionJDBC=new ConnectJDBC();
-
+        ConnectJDBC connectionJDBC=new ConnectJDBC();\
     }
 }
